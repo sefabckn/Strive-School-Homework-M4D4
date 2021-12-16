e23439ad4deb54425ad335d34a7552e543280dc3
SefaBckn <31974256+sefabckn@users.noreply.github.com>
2021-12-16 14:13:52 +0100

    previous project imported

---
+++ src/components/SingleBook.jsx
@@ -0,0 +1,32 @@
+import { Container, Row } from 'react-bootstrap'
+import Card from 'react-bootstrap/Card'
+import { Component } from 'react'
+class SingleBook extends Component{
+    state= {
+        selected: false
+    }
+    render(){
+        return(
+            <Container>
+                <Row>{
+                        <Card style={{ width: '12rem' }}>
+                            <Card.Img 
+                            variant="top" 
+                            src={this.props.book.img}
+                            onClick={()=> {this.setState({selected:!this.state.selected})}}//contolling state with each click. On second click value will change. true/false => false/true 
+                            style={{border:`2px solid ${this.state.selected?"red":"white"}`}}
+                            />
+                            <Card.Body>
+                                <Card.Title>{this.props.book.category}</Card.Title>
+                                <Card.Text>
+                                {this.props.book.title}
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>        
+                   
+                    }</Row>
+            </Container>
+        )
+    }
+   }
+export default SingleBook
